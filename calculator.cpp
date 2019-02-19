0ab4977f79e45e228c8af0eed3ba1a0d28a05d90
Aayush Kumar <you@example.com>
2019-02-19 16:56:30 +0530

    added two operation

--- calculator.cpp
+++ calculator.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 
 int main(){
-	int a,b;
+	int a,b,c,n;
 	cout<<enter two number;
 	cin>>a,b;
 	cout<<endl<<"1 for addition";
 	cout<<endl<<"2 for subtraction";
 	cout<<endl<<"3 for divide";
 	cout<<endl<<"4 for multiply";
+	cin<<c;
 	
-	
-	
+	switch(c)
+    case 1:
+    	n=a+b;
+    case 2:
+    	n=a-b;
+		
 	
 }
