19e596db4940c8ea370db87a56a7ea6eebe65d8b
Aayush Kumar <you@example.com>
2019-02-19 16:52:18 +0530

    enter numbers

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	cout<<enter two number;
+	cin>>a,b;
+	cout<<endl<<"1 for addition";
+	cout<<endl<<"2 for subtraction";
+	cout<<endl<<"3 for divide";
+	cout<<endl<<"4 for multiply";
+	
+	
+	
+	
+}
